047a5ac5b6bdc81e7167c958a8fea5a6ad31f9fb
aivilo-adamah <adamaholivia444@gmail.com>
2023-04-01 00:06:42 +0200

    rappel js

---
+++ App.js
@@ -0,0 +1,23 @@
+// let variableLet = "globale";
+// if(true){
+//   let variableLet = "locale";
+//   console.log("Let :" + variableLet);
+// }
+//   console.log("Let :" + variableLet);
+
+
+/*function direBonjour(prenom){
+  console.log("bonjour " + prenom);
+}
+ // fonction fléché
+let direBonsoir = function(){
+  console.log("saluuuuuuuuuut");
+}
+direBonsoir();*/
+// direBonjour('oliviacheri')
+
+let direAurevoir = ()=> "au revoir";
+console.log(direAurevoir());
+
+let Addition = (nbreA,nbreB) => nbreA+nbreB;
+console.log(Addition(3,8));
